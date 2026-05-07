f6367df8f8c61389ab8addc4be99827165729818
Homutov <maksimchomutov11@gmail.com>
2026-05-07 14:36:00 +0300

    konveer1.7

---
+++ lpm_shiftreg0_inst.vhd
@@ -0,0 +1,6 @@
+lpm_shiftreg0_inst : lpm_shiftreg0 PORT MAP (
+		clock	 => clock_sig,
+		enable	 => enable_sig,
+		shiftin	 => shiftin_sig,
+		q	 => q_sig
+	);
---
+++ lpm_shiftreg1_inst.vhd
@@ -0,0 +1,8 @@
+lpm_shiftreg1_inst : lpm_shiftreg1 PORT MAP (
+		clock	 => clock_sig,
+		data	 => data_sig,
+		enable	 => enable_sig,
+		load	 => load_sig,
+		shiftin	 => shiftin_sig,
+		q	 => q_sig
+	);
